57c9d4d1c2221a5a1be9177f97a6e51bbe6727dd
Gilbert <steven.n.gilbert@gmail.com>
2017-01-06 09:26:42 -0500

    🐷💁 Updated Uncle-Dimmy with Gomix

---
+++ public/client.js
@@ -0,0 +1,35 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+
+function initialize() {
+  let btn = document.getElementById("btn");
+  btn.onclick = handleClick;
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  let text = document.getElementById("word"); 
+  let inputWord = text.value;
+  console.log(inputWord);
+  let lastVowel = inputWord.charAt(inputWord.length - 1);
+  console.log(lastVowel);
+  let dimunutive = document.getElementById("dimunutive");
+  let newWord = "";
+  
+  if (typeof inputWord !== "string") {
+    console.log("Alert user to input a string");
+  } else {
+    if (lastVowel === "o") {
+      newWord = inputWord.slice(0, -1) + "ito";  
+      dimunutive.innerHTML = newWord;
+    } else if (lastVowel === "a") {
+      newWord = inputWord.slice(0, -1) + "ita";
+      dimunutive.innerHTML = newWord;
+    }
+  }
+}
+
+window.onload = initialize;
