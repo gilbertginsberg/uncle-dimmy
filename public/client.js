5ac5ac35eecd496d9925cf155e7f5fd793c18859
System <none>
2017-01-15 02:23:41 +0000

    💴👣 Corrected spelling of diminutive id

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 //globals
-var dimunutive = document.getElementById("dimunutive");
+var diminutive = document.getElementById("diminutive");
 var newWord = "";
 var specialIco = document.getElementById("specialIco");
 
@@ -47,40 +47,40 @@ function masculineForms(lastChar, inputWord, secondToLastChar) {
  
   if (secondToLastChar === "c") {
     newWord = inputWord.slice(0, -2) + "quito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "k") {
     newWord = inputWord.slice(0, -1) + "quito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (secondToLastChar === "g") {
     newWord = inputWord.slice(0, -1) + "uito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "z") {
     newWord = inputWord.slice(0, -1) + "cecito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (secondToLastChar === "u") {
     newWord = inputWord.slice(0, -2) + "üito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (secondToLastChar === "t") {
     newWord = "1: " + inputWord.slice(0, -1) + "ito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
     
     newWord = "2: " + inputWord.slice(0, -1) + "ico";  
     specialIco.innerHTML = newWord;
   } else if (lastChar === "o") {
     newWord = inputWord.slice(0, -1) + "ito";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "l" || lastChar === "s" && secondToLastChar === "e") {
     newWord = inputWord + "ito";
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "r" || lastChar === "n" || lastChar === "e") {
     newWord = inputWord + "cito";
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "s") {
     newWord = inputWord.slice(0, -2) + "itos";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else {
-     newWord = inputWord + "ito";
-     dimunutive.innerHTML = newWord;
+    newWord = inputWord + "ito";
+    diminutive.innerHTML = newWord;
   }
  }
 
@@ -89,47 +89,47 @@ function feminineForms(lastChar, inputWord, secondToLastChar) {
   
    if (secondToLastChar === "c") {
     newWord = inputWord.slice(0, -2) + "quita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "k") {
     newWord = inputWord.slice(0, -1) + "quita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (secondToLastChar === "g") {
     newWord = inputWord.slice(0, -1) + "uita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "z") {
     newWord = inputWord.slice(0, -1) + "cecita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (secondToLastChar === "u") {
     newWord = inputWord.slice(0, -2) + "üita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (secondToLastChar === "t") {
     newWord = "1: " + inputWord.slice(0, -1) + "ita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
     
     newWord = "2: " + inputWord.slice(0, -1) + "ica";  
     specialIco.innerHTML = newWord;
   } else if (lastChar === "a") {
     newWord = inputWord.slice(0, -1) + "ita";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "l" || lastChar === "s" && secondToLastChar === "e") {
     newWord = inputWord + "ita";
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "r" || lastChar === "n" || lastChar === "e") {
     newWord = inputWord + "cita";
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else if (lastChar === "s") {
     newWord = inputWord.slice(0, -2) + "itas";  
-    dimunutive.innerHTML = newWord;
+    diminutive.innerHTML = newWord;
   } else {
      newWord = inputWord + "ita";
-     dimunutive.innerHTML = newWord;
+     diminutive.innerHTML = newWord;
   }
  }
  
  function adverbForms (lastChar, inputWord, secondToLastChar) {
    if (lastChar === "a") {
      newWord = inputWord.slice(0, -1) + "ita";
-     dimunutive.innerHTML = newWord;
+     diminutive.innerHTML = newWord;
    } else {
      masculineForms(lastChar, inputWord, secondToLastChar);
    }
